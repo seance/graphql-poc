fa036ef75dbd6e3e010b11b63ec515e21cde9ec3
Jukka Viinamäki <jukka.viinamaki@futurice.com>
2016-11-23 17:19:21 +0200

    Embed info about stack in bootstrap data

--- backend-akka-postgres/db-bootstrap.sql
+++ backend-akka-postgres/db-bootstrap.sql
@@ -1,2 +1,2 @@
 CREATE TABLE "foos" ("id" SERIAL NOT NULL PRIMARY KEY,"bar" VARCHAR NOT NULL,"zut" INTEGER NOT NULL);
-INSERT INTO "foos" ("bar", "zut") VALUES ('hello', 42), ('world', 13);
+INSERT INTO "foos" ("bar", "zut") VALUES ('hello', 42), ('akka/postgres', 13);
